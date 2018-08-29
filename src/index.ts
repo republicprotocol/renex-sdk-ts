2adfb55d2665e2756d0c1d85929134a166e115a4
negaex <gh@negaex.com>
2018-08-29 12:10:32 +1000

    Added sdk interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
 import Web3 from "web3";
 import { Provider } from "web3/types";
+import { BN } from "bn.js";
 
-class RenExSDK {
+// Types not implemented yet
+export type IdempotentKey = null;
+export type OrderID = null;
+export type Order = null;
+export enum OrderStatus { }
+
+/**
+ * This is the interface that the SDK exposes.
+ *
+ * @interface RenExSDK
+ */
+interface RenExSDK {
+    address(): string;
+    transfer(address: string, token: number, value: BN);
+    balance(token: number): BN;
+    usableBalance(token: number): BN;
+    deposit(token: number, value: BN);
+    withdraw(token: number, value: BN, forced: boolean, key: IdempotentKey): IdempotentKey;
+    status(orderID: OrderID): OrderStatus;
+    settled(orderID: OrderID): boolean;
+    openOrder(order: Order);
+    cancelOrder(orderID: OrderID);
+    listOrdersByTrader(address: string): OrderID[];
+    listOrdersByStatus(status: OrderStatus): OrderID[];
+}
+
+/**
+ * This is the concrete class that implements the RenExSDK interface.
+ *
+ * @class RenExSDK
+ * @implements {RenExSDK}
+ */
+class RenExSDK implements RenExSDK {
     private web3;
 
     /**
@@ -13,3 +46,5 @@ class RenExSDK {
         this.web3 = new Web3(provider);
     }
 }
+
+export default RenExSDK;
