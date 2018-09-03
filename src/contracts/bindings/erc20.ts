4fe621d347d12e2b809ca75219e86b6091a930cd
negaex <gh@negaex.com>
2018-09-03 10:45:13 +1000

    Added missing typescript bindings files

---
+++ src/contracts/bindings/erc20.ts
@@ -0,0 +1,26 @@
+import { BN } from "bn.js";
+import { Log, TransactionReceipt, Tx } from "web3/types";
+
+export interface Transaction { receipt: TransactionReceipt; tx: string; logs: Log[]; }
+
+type BigNumber = string | number | BN;
+
+// tslint:disable:max-line-length
+export interface ERC20Contract {
+    totalSupply(options?: Tx): Promise<BigNumber>;
+    balanceOf(who: string, options?: Tx): Promise<BigNumber>;
+    transfer(to: string, value: BigNumber, options?: Tx): Promise<Transaction>;
+    allowance(owner: string, spender: string, options?: Tx): Promise<BigNumber>;
+    transferFrom(from: string, to: string, value: BigNumber, options?: Tx): Promise<Transaction>;
+    approve(spender: string, value: BigNumber, options?: Tx): Promise<Transaction>;
+    address: string;
+}
+
+export interface ERC20Artifact {
+    address: string;
+    "new"(options?: Tx): Promise<ERC20Contract>;
+    at(address: string): Promise<ERC20Contract>;
+    deployed(): Promise<ERC20Contract>;
+}
+
+// tslint:enable:max-line-length
---
+++ src/contracts/bindings/ren_ex_tokens.ts
@@ -0,0 +1,27 @@
+import { BN } from "bn.js";
+import { Log, TransactionReceipt, Tx } from "web3/types";
+
+export interface Transaction { receipt: TransactionReceipt; tx: string; logs: Log[]; }
+
+type BigNumber = string | number | BN;
+
+// tslint:disable:max-line-length
+export interface RenExTokensContract {
+    renounceOwnership(options?: Tx): Promise<Transaction>;
+    owner(options?: Tx): Promise<string>;
+    transferOwnership(_newOwner: string, options?: Tx): Promise<Transaction>;
+    tokens(index_0: number|BigNumber, options?: Tx): Promise<{ addr: string, decimals: BigNumber, registered: boolean,  0: string, 1: BigNumber, 2: boolean }>;
+    VERSION(options?: Tx): Promise<string>;
+    registerToken(_tokenCode: number|BigNumber, _tokenAddress: string, _tokenDecimals: number|BigNumber, options?: Tx): Promise<Transaction>;
+    deregisterToken(_tokenCode: number|BigNumber, options?: Tx): Promise<Transaction>;
+    address: string;
+}
+
+export interface RenExTokensArtifact {
+    address: string;
+    "new"(_VERSION: string, options?: Tx): Promise<RenExTokensContract>;
+    at(address: string): Promise<RenExTokensContract>;
+    deployed(): Promise<RenExTokensContract>;
+}
+
+// tslint:enable:max-line-length
