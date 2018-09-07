aead57cdd0ee2ad635c4deaa8d19a2697d3d69d5
Negaex <gh@negaex.com>
2018-09-07 12:40:55 +1000

    Convert base64 to hex in status()

--- src/methods/settlementMethods.ts
+++ src/methods/settlementMethods.ts
@@ -3,12 +3,16 @@ import { BN } from "bn.js";
 import RenExSDK, { OrderID, OrderStatus } from "..//index";
 
 import { Orderbook, RenExSettlement, withProvider } from "../contracts/contracts";
+import { EncodedData, Encodings } from "../lib/encodedData";
 import { NetworkData } from "../lib/network";
 import { orderbookStateToOrderStatus, settlementStatusToOrderStatus } from "../lib/order";
 
-export const status = async (sdk: RenExSDK, orderID: OrderID): Promise<OrderStatus> => {
+export const status = async (sdk: RenExSDK, orderID64: OrderID): Promise<OrderStatus> => {
     sdk.contracts.orderbook = sdk.contracts.orderbook || await withProvider(sdk.web3, Orderbook).at(NetworkData.contracts[0].orderbook);
     sdk.contracts.renExSettlement = sdk.contracts.renExSettlement || await withProvider(sdk.web3, RenExSettlement).at(NetworkData.contracts[0].renExSettlement);
+
+    // Convert orderID from base64 to hex
+    const orderID = new EncodedData(orderID64, Encodings.BASE64).toHex();
 
     const obStatus = orderbookStateToOrderStatus(new BN(await sdk.contracts.orderbook.orderState(orderID)).toNumber());
     switch (obStatus) {
