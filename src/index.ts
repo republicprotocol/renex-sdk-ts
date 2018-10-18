e11066ec6a8acfc6a2c608643478af78c93385b1
Vince Au <vinceau09@gmail.com>
2018-10-18 13:43:47 +1100

    Fix optional parameter

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ class RenExSDK {
 
     public utils = {
         normalizePrice: (price: BigNumber): BigNumber => normalizePrice(price),
-        normalizeVolume: (volume: BigNumber, roundDown: boolean): BigNumber => normalizeVolume(volume, roundDown),
+        normalizeVolume: (volume: BigNumber, roundDown?: boolean): BigNumber => normalizeVolume(volume, roundDown),
         getTokenPriority: (token: TokenCode): number => tokenToID(token),
     };
 
