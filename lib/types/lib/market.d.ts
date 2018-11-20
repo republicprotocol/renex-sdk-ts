422dc36da0cccc7432cc8e192abe97cdde9acd00
Vince Au <vinceau09@gmail.com>
2018-11-20 17:01:16 +1100

    Remove lib folder

--- lib/types/lib/market.d.ts
+++
@@ -1,5 +0,0 @@
-import RenExSDK from "../index";
-import { OrderedMap } from "immutable";
-import { MarketCode, MarketDetails } from "../types";
-export declare const MarketPairs: OrderedMap<MarketCode, MarketDetails>;
-export declare function fetchMarkets(sdk: RenExSDK): Promise<MarketDetails[]>;
