83f4d9a20763f482cf12ccfb7e759b3899cc37ca
Vince Au <vinceau09@gmail.com>
2019-01-04 11:28:42 +1100

    Explicitly create the TraderOrder object when deserializing

--- src/migration/migration.ts
+++ src/migration/migration.ts
@@ -1,24 +1,23 @@
 import { fromSmallestUnit } from "../lib/tokens";
-import { TraderOrder } from "../types";
-import { deserializeV0TraderOrder, idToToken, OrderSettlementMapper, OrderSideMapper, OrderTypeMapper, tokenToDigits, V0TraderOrder } from "./version0Types";
+import { MatchDetails, TraderOrder } from "../types";
+import { deserializeV0TraderOrder, idToToken, OrderSettlementMapper, OrderSideMapper, OrderStatusMapper, OrderTypeMapper, tokenToDigits, V0TraderOrder } from "./version0Types";
 
 // Migrate stored orders for backwards compatibility
 export function migrateV0toV1(orderString: string): TraderOrder {
-    const newOrder: TraderOrder = JSON.parse(orderString);
-    if (newOrder.version === 1) {
-        return newOrder;
+    let matchDetails: MatchDetails | undefined;
+    const parsedOrder = JSON.parse(orderString);
+    if (parsedOrder.version === 1) {
+        return parsedOrder;
     }
-    newOrder.version = 1;
 
     const order: V0TraderOrder = deserializeV0TraderOrder(orderString);
-
     if (order.matchDetails) {
         const receivedToken = idToToken(order.matchDetails.receivedToken);
         const spentToken = idToToken(order.matchDetails.spentToken);
         const fee = fromSmallestUnit(order.matchDetails.fee.toString(), tokenToDigits(receivedToken));
         const receivedVolume = fromSmallestUnit(order.matchDetails.receivedVolume.toString(), tokenToDigits(receivedToken));
         const spentVolume = fromSmallestUnit(order.matchDetails.spentVolume.toString(), tokenToDigits(spentToken));
-        newOrder.matchDetails = {
+        matchDetails = {
             orderID: order.matchDetails.orderID,
             matchedID: order.matchDetails.matchedID,
             receivedVolume,
@@ -32,22 +31,35 @@ export function migrateV0toV1(orderString: string): TraderOrder {
     const receiveToken = idToToken(order.orderInputs.receiveToken);
     const spendToken = idToToken(order.orderInputs.spendToken);
     const feeToken = idToToken(order.computedOrderDetails.feeToken);
-    newOrder.computedOrderDetails.receiveToken = receiveToken;
-    newOrder.computedOrderDetails.spendToken = spendToken;
-    newOrder.computedOrderDetails.receiveVolume = fromSmallestUnit(order.computedOrderDetails.receiveVolume.toString(), tokenToDigits(receiveToken));
-    newOrder.computedOrderDetails.spendVolume = fromSmallestUnit(order.computedOrderDetails.spendVolume.toString(), tokenToDigits(spendToken));
-    newOrder.computedOrderDetails.feeAmount = fromSmallestUnit(order.computedOrderDetails.feeAmount.toString(), tokenToDigits(feeToken));
-    newOrder.computedOrderDetails.feeToken = feeToken;
-    newOrder.computedOrderDetails.orderSettlement = OrderSettlementMapper(order.orderInputs.orderSettlement);
-    newOrder.computedOrderDetails.nonce = order.orderInputs.nonce;
-
     const nonPriorityToken = order.orderInputs.receiveToken < order.orderInputs.spendToken ? order.orderInputs.spendToken : order.orderInputs.receiveToken;
     const priorityToken = order.orderInputs.receiveToken > order.orderInputs.spendToken ? order.orderInputs.spendToken : order.orderInputs.receiveToken;
-    newOrder.orderInputs.symbol = `${nonPriorityToken}/${priorityToken}`;
-    newOrder.orderInputs.side = OrderSideMapper(order.computedOrderDetails.parity);
-    newOrder.orderInputs.volume = fromSmallestUnit(order.orderInputs.volume.toString(), tokenToDigits(idToToken(nonPriorityToken)));
-    newOrder.orderInputs.minVolume = fromSmallestUnit(order.orderInputs.minimumVolume.toString(), tokenToDigits(idToToken(nonPriorityToken)));
-    newOrder.orderInputs.type = OrderTypeMapper(order.orderInputs.type);
 
+    const newOrder: TraderOrder = {
+        version: 1,
+        id: order.id,
+        trader: order.trader,
+        status: OrderStatusMapper(order.status),
+        matchDetails,
+        computedOrderDetails: {
+            receiveToken,
+            spendToken,
+            receiveVolume: fromSmallestUnit(order.computedOrderDetails.receiveVolume.toString(), tokenToDigits(receiveToken)),
+            spendVolume: fromSmallestUnit(order.computedOrderDetails.spendVolume.toString(), tokenToDigits(spendToken)),
+            date: order.computedOrderDetails.date,
+            feeAmount: fromSmallestUnit(order.computedOrderDetails.feeAmount.toString(), tokenToDigits(feeToken)),
+            feeToken,
+            orderSettlement: OrderSettlementMapper(order.orderInputs.orderSettlement),
+            nonce: order.orderInputs.nonce,
+        },
+        orderInputs: {
+            symbol: `${idToToken(nonPriorityToken)}/${idToToken(priorityToken)}`,
+            side: OrderSideMapper(order.computedOrderDetails.parity),
+            price: order.orderInputs.price,
+            volume: fromSmallestUnit(order.orderInputs.volume.toString(), tokenToDigits(idToToken(nonPriorityToken))),
+            minVolume: fromSmallestUnit(order.orderInputs.minimumVolume.toString(), tokenToDigits(idToToken(nonPriorityToken))),
+            type: OrderTypeMapper(order.orderInputs.type),
+        },
+        transactionHash: order.transactionHash,
+    };
     return newOrder;
 }
