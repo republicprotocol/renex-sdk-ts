770d424a43508f043d258fbae981a2fba1fde53b
Negaex <gh@negaex.com>
2019-01-24 10:47:20 +1100

    Updated network configs

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -4,6 +4,8 @@ import testnet from "./networks/testnet.json";
 export interface NetworkData {
     network: string;
     ingress: string;
+    infura: string;
+    etherscan: string;
     wbtcKYCServer: string;
     ethNetwork: string;
     ethNetworkLabel: string;
