4cafd7f6769beb9d838c32b8c73c2056e003f08f
Negaex <gh@negaex.com>
2019-01-24 10:28:10 +1100

    Fixed missing MarketPairs entries

--- src/lib/market.ts
+++ src/lib/market.ts
@@ -4,9 +4,11 @@ import { OrderedMap } from "immutable";
 import { MarketCode, MarketDetails, MarketPair, OrderSettlement, Token } from "../types";
 
 export const MarketPairs: OrderedMap<MarketCode, MarketDetails> = OrderedMap<MarketCode, MarketDetails>()
-    // RenExAtomic:
+    // BTC pairs
     .set(MarketPair.ETH_BTC, { symbol: MarketPair.ETH_BTC, orderSettlement: OrderSettlement.RenExAtomic, quote: Token.BTC, base: Token.ETH })
-    // RenEx:
+    .set(MarketPair.REN_BTC, { symbol: MarketPair.REN_BTC, orderSettlement: OrderSettlement.RenExAtomic, quote: Token.BTC, base: Token.ETH })
+    .set(MarketPair.TUSD_BTC, { symbol: MarketPair.TUSD_BTC, orderSettlement: OrderSettlement.RenExAtomic, quote: Token.BTC, base: Token.ETH })
+    // ETH pairs
     .set(MarketPair.DGX_ETH, { symbol: MarketPair.DGX_ETH, orderSettlement: OrderSettlement.RenExAtomic, quote: Token.ETH, base: Token.DGX })
     .set(MarketPair.TUSD_ETH, { symbol: MarketPair.TUSD_ETH, orderSettlement: OrderSettlement.RenExAtomic, quote: Token.ETH, base: Token.TUSD })
     .set(MarketPair.REN_ETH, { symbol: MarketPair.REN_ETH, orderSettlement: OrderSettlement.RenExAtomic, quote: Token.ETH, base: Token.REN })
