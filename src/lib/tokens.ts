7af3eb86c3da166f72b3acc4de60fe98b2f2462f
Vince Au <vinceau09@gmail.com>
2019-03-25 17:49:46 +1100

    Add ZEC

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -11,6 +11,7 @@ export enum Token {
     REN = "REN",
     TUSD = "TUSD",
     WBTC = "WBTC",
+    ZEC = "ZEC",
 }
 
 export const Tokens = new Map<Token, TokenDetails>()
@@ -19,7 +20,8 @@ export const Tokens = new Map<Token, TokenDetails>()
     .set(Token.ETH, { symbol: Token.ETH, name: "Ethereum", decimals: 18, priority: 1024 })
     .set(Token.REN, { symbol: Token.REN, name: "Ren", decimals: 18, priority: 1025 })
     .set(Token.TUSD, { symbol: Token.TUSD, name: "TrueUSD", decimals: 18, priority: 1026 })
-    .set(Token.WBTC, { symbol: Token.WBTC, name: "Wrapped Bitcoin", decimals: 8, priority: 201 })
+    .set(Token.WBTC, { symbol: Token.WBTC, name: "Wrapped Bitcoin", decimals: 8, priority: 1033 })
+    .set(Token.ZEC, { symbol: Token.ZEC, name: "Zcash", decimals: 8, priority: 201 })
     ;
 
 export enum MarketPair {
@@ -27,10 +29,12 @@ export enum MarketPair {
     ETH_DAI = "ETH/DAI",
     REN_DAI = "REN/DAI",
     TUSD_DAI = "TUSD/DAI",
+    ZEC_DAI = "ZEC/DAI",
 
     ETH_BTC = "ETH/BTC",
     REN_BTC = "REN/BTC",
     TUSD_BTC = "TUSD/BTC",
+    ZEC_BTC = "ZEC/BTC",
     WBTC_BTC = "BTC/WBTC",
 }
 
@@ -40,11 +44,13 @@ export const MarketPairs = OrderedMap<MarketPair, MarketDetails>()
     .set(MarketPair.ETH_DAI, { symbol: MarketPair.ETH_DAI, quote: Token.DAI, base: Token.ETH })
     .set(MarketPair.REN_DAI, { symbol: MarketPair.REN_DAI, quote: Token.DAI, base: Token.REN })
     .set(MarketPair.TUSD_DAI, { symbol: MarketPair.TUSD_DAI, quote: Token.DAI, base: Token.TUSD })
+    .set(MarketPair.ZEC_DAI, { symbol: MarketPair.ZEC_DAI, quote: Token.DAI, base: Token.ZEC })
 
     // BTC pairs
     .set(MarketPair.ETH_BTC, { symbol: MarketPair.ETH_BTC, quote: Token.BTC, base: Token.ETH })
     .set(MarketPair.REN_BTC, { symbol: MarketPair.REN_BTC, quote: Token.BTC, base: Token.REN })
     .set(MarketPair.TUSD_BTC, { symbol: MarketPair.TUSD_BTC, quote: Token.BTC, base: Token.TUSD })
+    .set(MarketPair.ZEC_BTC, { symbol: MarketPair.ZEC_BTC, quote: Token.BTC, base: Token.ZEC })
     .set(MarketPair.WBTC_BTC, { symbol: MarketPair.WBTC_BTC, quote: Token.BTC, base: Token.WBTC })
     ;
 
